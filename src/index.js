9ed19c0f53512663282dd8e6e2a9bf48e8a3e28e
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-08-20 18:21:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
 // import { App } from "./src/App.js"
 
 // ReactDOM.render(<App />, document.getElementById("root"))
 
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container);
 
-const element = React.createElement('div', {className: 'div_name'}, 
-          React.createElement('h1', {className: 'div_name'}, 'Header Text'), 
-          React.createElement('p', null, 'Paragraph Text'));
+// const element = React.createElement('div', {className: 'div_name'}, 
+//           React.createElement('h1', {className: 'div_name'}, 'Header Text'), 
+//           React.createElement('p', null, 'Paragraph Text'));
 
-root.render(element);
+// root.render(element);
+
+Text
