8bec499a779462e5aef99f7d19aea58ee11cc9f9
CodeSolutions2 <j622amilah@gmail.com>
2024-08-20 09:54:29 +0000

    Updates

---
+++ App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <h1>Title</h1>
+      <ul>
+        <li>Item 0</li>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,6 @@
+# temp_repo_GithubPagesDeploymentTests
+
+
+A test repository to deploy GitHub Pages using GitHub Actions.
+
+- *In progress*, deploy a React webapp using GitHub Actions.
