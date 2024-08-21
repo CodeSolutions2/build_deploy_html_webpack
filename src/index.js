d63225b3126cfad8fc3a57d60f0c37f825729f99
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-08-21 17:40:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
+// import React from "react";
+// import { createRoot } from 'react-dom/client';
 
 // import { App } from "./src/App.js"
 
 // -----------------------------------------
 
 // [0] Building-Deploying JavaScript code using Node.js: WORKED
-// const helloDiv = document.createElement("div");
-// helloDiv.innerHTML = "Hello from Node.js!";
-// document.body.append(helloDiv);
+const helloDiv = document.createElement("div");
+helloDiv.innerHTML = "Hello from Node.js!";
+document.body.append(helloDiv);
 
 // -----------------------------------------
 
 // [1] Building-Deploying React code using Node.js: 
-const container = document.getElementById('root');
-const root = createRoot(container);
-const element = React.createElement('div', {className: 'div_name'}, 
-                                    React.createElement('h1', {className: 'div_name'}, 'Header Text'), 
-                                    React.createElement('p', null, 'Paragraph Text'));
-root.render(element);
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// const element = React.createElement('div', {className: 'div_name'}, 
+//                                     React.createElement('h1', {className: 'div_name'}, 'Header Text'), 
+//                                     React.createElement('p', null, 'Paragraph Text'));
+// root.render(element);
 // OR
 // ReactDOM.render(<App />, container)
