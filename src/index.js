6d35afe61d8c6d3c431d48b9e40dfe4e4a3e1a21
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-08-21 15:17:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 // import { App } from "./src/App.js"
 
@@ -14,7 +14,7 @@ import ReactDOM from "react-dom";
 
 // [1] Building-Deploying React code using Node.js: 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 const element = React.createElement('div', {className: 'div_name'}, 
                                     React.createElement('h1', {className: 'div_name'}, 'Header Text'), 
                                     React.createElement('p', null, 'Paragraph Text'));
