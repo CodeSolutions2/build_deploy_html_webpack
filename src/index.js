119661fbc1b96ac26aee707fe966c4e51528a66b
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-08-23 13:47:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,10 +17,12 @@ import { createRoot } from 'react-dom/client';
 // [1] Building-Deploying React code using Node.js: 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const element = React.createElement('div', {className: 'div_name'}, 
-                                    React.createElement('h1', {className: 'h1_element'}, 'Header Text'), 
+const element = React.createElement('div', {className: 'div_element'}, 
+                                    React.createElement('h1', {className: 'h1_element', onLoadStart: () => {const random_HEX_color = () =>  `#${Math.random().toString(16).slice(2,8)}`; document.querySelector('.h1_element').style.color = random_HEX_color;}}, 'Header Text'), 
                                     React.createElement('p', {className: 'p_element'}, 'Paragraph Text'));
 
+
+                                                        
 root.render(element);
 // OR
 // ReactDOM.render(<App />, container)
