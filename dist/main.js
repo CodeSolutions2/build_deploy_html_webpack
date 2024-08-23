495ec9b7f8c4f9a459dc37cd5d618defc4907e1d
CodeSolutions2 <j622amilah@gmail.com>
2024-08-23 13:50:49 +0000

    Updating repository

--- dist/main.js
+++ dist/main.js
@@ -33401,6 +33401,131 @@ if (
 }
 
 
+/***/ }),
+/* 8 */
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(10);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, `.h1_element {
+  color: red;
+}
+`, ""]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+/* 9 */
+/***/ ((module) => {
+
+
+
+module.exports = function (i) {
+  return i[1];
+};
+
+/***/ }),
+/* 10 */
+/***/ ((module) => {
+
+
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+*/
+module.exports = function (cssWithMappingToString) {
+  var list = [];
+
+  // return the list of modules as css string
+  list.toString = function toString() {
+    return this.map(function (item) {
+      var content = "";
+      var needLayer = typeof item[5] !== "undefined";
+      if (item[4]) {
+        content += "@supports (".concat(item[4], ") {");
+      }
+      if (item[2]) {
+        content += "@media ".concat(item[2], " {");
+      }
+      if (needLayer) {
+        content += "@layer".concat(item[5].length > 0 ? " ".concat(item[5]) : "", " {");
+      }
+      content += cssWithMappingToString(item);
+      if (needLayer) {
+        content += "}";
+      }
+      if (item[2]) {
+        content += "}";
+      }
+      if (item[4]) {
+        content += "}";
+      }
+      return content;
+    }).join("");
+  };
+
+  // import a list of modules into the list
+  list.i = function i(modules, media, dedupe, supports, layer) {
+    if (typeof modules === "string") {
+      modules = [[null, modules, undefined]];
+    }
+    var alreadyImportedModules = {};
+    if (dedupe) {
+      for (var k = 0; k < this.length; k++) {
+        var id = this[k][0];
+        if (id != null) {
+          alreadyImportedModules[id] = true;
+        }
+      }
+    }
+    for (var _k = 0; _k < modules.length; _k++) {
+      var item = [].concat(modules[_k]);
+      if (dedupe && alreadyImportedModules[item[0]]) {
+        continue;
+      }
+      if (typeof layer !== "undefined") {
+        if (typeof item[5] === "undefined") {
+          item[5] = layer;
+        } else {
+          item[1] = "@layer".concat(item[5].length > 0 ? " ".concat(item[5]) : "", " {").concat(item[1], "}");
+          item[5] = layer;
+        }
+      }
+      if (media) {
+        if (!item[2]) {
+          item[2] = media;
+        } else {
+          item[1] = "@media ".concat(item[2], " {").concat(item[1], "}");
+          item[2] = media;
+        }
+      }
+      if (supports) {
+        if (!item[4]) {
+          item[4] = "".concat(supports);
+        } else {
+          item[1] = "@supports (".concat(item[4], ") {").concat(item[1], "}");
+          item[4] = supports;
+        }
+      }
+      list.push(item);
+    }
+  };
+  return list;
+};
+
 /***/ })
 /******/ 	]);
 /************************************************************************/
@@ -33487,10 +33612,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(8);
 
 
-// import "./style.css";
-// import * as css from "style.css";
+
+//import * as css from "./style.css";
 
 // import { App } from "./src/App.js"
 
@@ -33503,14 +33629,36 @@ __webpack_require__.r(__webpack_exports__);
 
 // -----------------------------------------
 
-// [1] Building-Deploying React code using Node.js: 
+// [1] Building-Deploying React code using Node.js with JavaScript notation: 
 const container = document.getElementById('root');
 const root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot)(container);
+
+// Try 1: change color with onLoadStart
 const element = react__WEBPACK_IMPORTED_MODULE_0___default().createElement('div', {className: 'div_element'}, 
                                     react__WEBPACK_IMPORTED_MODULE_0___default().createElement('h1', {className: 'h1_element', onLoadStart: (event) => {const random_HEX_color = () =>  `#${Math.random().toString(16).slice(2,8)}`; event.target.style.color = random_HEX_color;}}, 'Header Text'), 
                                     react__WEBPACK_IMPORTED_MODULE_0___default().createElement('p', {className: 'p_element'}, 'Paragraph Text'));
+// RESULT: text color does not change
 
 
+// Try 0: change color here
+// const random_HEX_color = () =>  `#${Math.random().toString(16).slice(2,8)}`;
+// document.querySelector('.h1_element').style.color = random_HEX_color;
+// RESULT: Error, could not recognize className h1_element
+
+// -----------------------------------------
+
+// [2] Building-Deploying React code using Node.js notation: 
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+
+// const element = (
+//         <div className="div_element">
+//             <h1 className="h1_element" onLoadStart: (event) => {const random_HEX_color = () =>  `#${Math.random().toString(16).slice(2,8)}`; event.target.style.color = random_HEX_color;}>Header Text</h1>
+//             <p className="p_element">Paragraph Text</p>
+//         </div>
+// )
+
+// -----------------------------------------
                                                         
 root.render(element);
 // OR
