da616c6dfc9446b605fe24d586887c41e46c2c04
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-08-23 14:36:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,14 +14,34 @@ import { createRoot } from 'react-dom/client';
 
 // -----------------------------------------
 
-// [1] Building-Deploying React code using Node.js: 
+// [1] Building-Deploying React code using Node.js with JavaScript notation: 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const element = React.createElement('div', {className: 'div_element'}, 
-                                    React.createElement('h1', {className: 'h1_element', onLoadStart: (event) => {const random_HEX_color = () =>  `#${Math.random().toString(16).slice(2,8)}`; event.target.style.color = random_HEX_color;}}, 'Header Text'), 
-                                    React.createElement('p', {className: 'p_element'}, 'Paragraph Text'));
+
+// Try 1: change color with onLoadStart
+// const element = React.createElement('div', {className: 'div_element'}, 
+//                                     React.createElement('h1', {className: 'h1_element', onLoadStart: (event) => {const random_HEX_color = () =>  `#${Math.random().toString(16).slice(2,8)}`; event.target.style.color = random_HEX_color;}}, 'Header Text'), 
+//                                     React.createElement('p', {className: 'p_element'}, 'Paragraph Text'));
+// RESULT: text color does not change
 
 
+// Try 0: change color here
+// const random_HEX_color = () =>  `#${Math.random().toString(16).slice(2,8)}`;
+// document.querySelector('.h1_element').style.color = random_HEX_color;
+// RESULT: Error, could not recognize className h1_element
+
+// -----------------------------------------
+
+// [2] Building-Deploying React code using Node.js notation: 
+
+const element = (<div className="div_element">
+            <h1 
+                className='h1_element'
+            />
+        </div>
+)
+
+// -----------------------------------------
                                                         
 root.render(element);
 // OR
